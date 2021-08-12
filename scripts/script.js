1bd25febac315957c744046f1da447ffb94e4c54
V-Le-D <le.victor@hotmail.com>
2021-08-12 16:47:18 -0500

    Refactored and added equals functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,9 @@
 //https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/calculator
-const displayArea = document.querySelector('#display-area');
+const inputArea = document.querySelector('#inputted-area');
+const calcArea = document.querySelector('#calc-area');
+
 const buttonsContainer = document.querySelector('.buttons-container');
-const buttonClear = buttonsContainer.querySelector('#button-clear');
-
-const button7 = buttonsContainer.querySelector('#button-7');
-const button8 = buttonsContainer.querySelector('#button-8');
-const button9 = buttonsContainer.querySelector('#button-9');
-const buttonDivide = buttonsContainer.querySelector('#button-divide');
-
-const button4 = buttonsContainer.querySelector('#button-4');
-const button5 = buttonsContainer.querySelector('#button-5');
-const button6 = buttonsContainer.querySelector('#button-6');
-const buttonMultiply = buttonsContainer.querySelector('#button-multiply');
-
-const button1 = buttonsContainer.querySelector('#button-1');
-const button2 = buttonsContainer.querySelector('#button-2');
-const button3 = buttonsContainer.querySelector('#button-3');
-const buttonMinus = buttonsContainer.querySelector('#button-minus');
-
-const button0 = buttonsContainer.querySelector('#button-0');
-const buttonDecimal = buttonsContainer.querySelector('#button-decimal');
-const buttonPlus = buttonsContainer.querySelector('#button-plus');
-const buttonEqual = buttonsContainer.querySelector('#button-equal');
+const buttons = Array.from(buttonsContainer.querySelectorAll('.button'));
 
 function sumOf(x, y) {
     return x + y;
@@ -43,53 +25,44 @@ function calcEquals(calcString) {
     return calcString == '' ? 0 : new Function('return ' + calcString)();
 };
 
-var displayNum = '';
-var calcString = '';
-var tempCalcNum = '';
+var tempOne = '';
+var tempTwo = '';
+var tempOperator = '';
+let finalAnswer = '';
 
+buttons.map(function(button) {
+    button.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case "AC":
+                inputArea.innerText = '';
+                calcArea.innerText = '';
+                tempOne = '';
+                tempTwo = '';
+                break;
 
-button1.addEventListener('click', function() {
-    displayNum += '1';
-    calcString += '1';
-    displayArea.innerText = displayNum;
-    
-});
+            case "+":
+                if (inputArea.innerText == finalAnswer) {
+                    calcArea.innerText = finalAnswer + ' + ';
+                    inputArea.innerText = '';
+                    break;
+                } else {
+                    calcArea.innerText += inputArea.innerText + ' + ';
+                    inputArea.innerText = '';
+                    break;
+                }
 
-button2.addEventListener('click', function() {
-    displayNum += '2';
-    calcString += '2';
-    displayArea.innerText = displayNum;
-});
+            case "=":
+                tempTwo = calcArea.innerText + inputArea.innerText;
+                calcArea.innerText += ' ' + inputArea.innerText;
+                finalAnswer = calcEquals(tempTwo)
+                inputArea.innerText = finalAnswer;
+                break;
 
-buttonPlus.addEventListener('click', function() {
-    if(calcString == '' || isNaN(calcString[calcString.length -1])) {
-        return;
-    }
-
-    if (tempCalcNum = calcEquals(calcString)){
-        calcString = tempCalcNum;
-    }
-    displayNum = '';
-    calcString += '+';
-    displayArea.innerText = '+';
-    }
-);
-
-buttonEqual.addEventListener('click', function() {
-    if(displayNum == calcEquals(calcString)) {
-        return;
-    }
-    displayNum = '';
-    displayArea.innerText = calcEquals(calcString);
-    
-    tempCalcNum = calcEquals(calcString);
-    calcString =  '';
-});
+            default:
+                inputArea.innerText += e.target.innerText;
+        }
 
 
 
-buttonClear.addEventListener('click', function() {
-    displayNum = '';
-    calcString = '';
-    displayArea.innerText = '0';
-});
+    });
+})
